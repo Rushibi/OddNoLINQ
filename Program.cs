3b50b04b6d2b1bb9155ac6237bcee7e0953c4a69
Rushibi <rushibi1996@gmail.com>
2024-10-18 17:32:11 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace OddNoLINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n1 = new int[10] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            var nQuery =
+                from VrNum in n1
+                where (VrNum % 2) != 0
+                select VrNum;
+
+            foreach (int VrNum in nQuery)
+            {
+                Console.Write("{0} ", VrNum);
+            }
+            Console.Write("\n");
+        }
+    }
+}
